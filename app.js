78df56d2e3faf484e4f028500a798c305260b05f
GabrielFigueroa22 <gabrielfigueroa08@aragon.unam.mx>
2024-08-26 10:46:05 -0600

    Tarea

---
+++ app.js
@@ -0,0 +1,5 @@
+let nombre = prompt('Mi nombre es Gabriel, y el tuyo?');
+let edad = parseInt(prompt('tengo 21 años, y tu?'));
+
+console.log('el nombre es',nombre);
+console,log('la edad es',edad);
